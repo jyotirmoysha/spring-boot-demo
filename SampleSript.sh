2ba095ffcecc03b8ab9f69b647a682f1010cac17
Jyotirmoy Sarmah <jyotirmoysha@gmail.com>
2020-04-08 11:24:19 +0530

    test

--- SampleSript.sh
+++ SampleSript.sh
@@ -3,3 +3,4 @@
 #!/bin/sh
 echo "Hello Jyotirmoy, Sample Shell Script Exeuted successfully"
 kill $(cat ./pid.file)
+nohup java -jar /home/ec2-user/test/target/com.spring-boot.demo-0.0.1-SNAPSHOT.jar & echo $! > ./pid.file &
